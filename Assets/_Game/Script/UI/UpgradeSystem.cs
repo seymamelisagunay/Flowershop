40d22b4352d627daec818f59e59e19d2ac2e7f89
melisagunay <51082351+melisagunay@users.noreply.github.com>
2022-07-03 21:24:26 +0300

    add upgrade system

--- Assets/_Game/Script/UI/UpgradeSystem.cs
+++ Assets/_Game/Script/UI/UpgradeSystem.cs
@@ -1,53 +1,110 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 public class UpgradeSystem : MonoBehaviour
 {
-    public TMP_Text playerUpgrade;
-    public TMP_Text carryUpgrade;
-    public TMP_Text shelverUpgrade;
-    public TMP_Text perfumeUpgrade;
-    public TMP_Text delightUpgrade;
+    public GameObject moneyBox;
+    public TMP_Text playerUpgradeLevelName;
+    public TMP_Text playerUpgradeMoney;
+    public TMP_Text shelverUpgradeLevelName;
+    public TMP_Text shelverUpgradeMoney;
+    public TMP_Text carryUpgradeLevelName;
+    public TMP_Text carryUpgradeMoney;
+    public TMP_Text perfumeUpgradeLevelName;
+    public TMP_Text perfumeUpgradeMoney;
+    public TMP_Text delightUpgradeLevelName;
+    public TMP_Text delightUpgradeMoney;
 
-    enum PlayerUpgradeMoney { Lvl1 = 100, Lvl2 = 300, Lvl3 = 500, Lvl4 = 1000, Lvl5 = 10000 };
-    enum CarryUpgradeMoney { Lvl1 = 200, Lvl2 = 600, Lvl3 = 1500, Lvl4 = 3000 };
-    enum ShelverUpgradeMoney { Lvl1 = 1500, Lvl2 = 3000, Lvl3 = 6000, Lvl4 = 12000};
-    enum PerfumeUpgradeMoney { Lvl1 = 200, Lvl2 = 600, Lvl3 = 1500};
-    enum DelightUpgradeMoney { Lvl1 = 300, Lvl2 = 900, Lvl3 = 2100};
+    //upgrade ucretleri
+    public int[] PlayerUpgradeMoneyArray = {100, 300, 500, 1000, 10000 };
+    public int[] ShelverUpgradeMoneyArray = {200, 600, 1500, 3000 };
+    public int[] CarryUpgradeMoneyArray = {1500, 3000, 6000, 12000};
+    public int[] PerfumeUpgradeMoneyArray = {200,600,1500};
+    public int[] DelightUpgradeMoneyArray = {300,900,2100};
     
     private string _upgradeLevelName = "Upgrade Lvl. ";
+   
+    //bu kismmi kaydetmem lazim***. upgrade seviyelerimizi tutucak
+    private int _playerUpgradeLevel = 1;
+    private int _carryUpgradeLevel = 1;
+    private int _shelverUpgradeLevel = 1;
+    private int _perfumeUpgradeLevel = 1;
+    private int _delightUpgradeLevel = 1;
 
-    private int playerUpgradeLevel = 0;
-    private int _carryUpgradeLevel = 0;
-    private int _shelverUpgradeLevel = 0;
-    private int _perfumeUpgradeLevel = 0;
-    private int _delightUpgradeLevel = 0;
-    
+    public void Start() //baslangicta level ismimizi ve upgrade paralarini atiyoruz 
+    {
+        playerUpgradeLevelName.text = _upgradeLevelName + _playerUpgradeLevel;
+        playerUpgradeMoney.text = PlayerUpgradeMoneyArray[_playerUpgradeLevel - 1].ToString();
+        shelverUpgradeLevelName.text = _upgradeLevelName + _shelverUpgradeLevel;
+        shelverUpgradeMoney.text = ShelverUpgradeMoneyArray[_shelverUpgradeLevel - 1].ToString();
+        carryUpgradeLevelName.text = _upgradeLevelName + _carryUpgradeLevel;
+        carryUpgradeMoney.text = CarryUpgradeMoneyArray[_carryUpgradeLevel - 1].ToString();
+        perfumeUpgradeLevelName.text = _upgradeLevelName + _perfumeUpgradeLevel;
+        perfumeUpgradeMoney.text = PerfumeUpgradeMoneyArray[_perfumeUpgradeLevel - 1].ToString();
+        delightUpgradeLevelName.text = _upgradeLevelName + _delightUpgradeLevel;
+        delightUpgradeMoney.text = DelightUpgradeMoneyArray[_delightUpgradeLevel - 1].ToString();
+    }
+
+    public int UpgradeButton(int[] UpgradeMoneyArray, int upgradeLevel, TMP_Text upgradeLevelName, TMP_Text upgradeMoney)
+    {
+        int money = UpgradeMoneyArray[upgradeLevel - 1];
+        if (UserManager.Instance.CheckedMoney(money)) // paramiz varsa upgrade islemlerini yapicaz
+        {
+            if (upgradeLevel >= UpgradeMoneyArray.Length) 
+            {
+                UserManager.Instance.DecreasingMoney(money);
+                upgradeMoney.GetComponentInParent(typeof(Button)).gameObject.SetActive(false);//eger tum upgradeler yapilirsa butonu gorunmez yapiyoruz
+                upgradeLevelName.text = "Upgrade Lvl. Max";
+                
+            }
+            else
+            {
+                UserManager.Instance.DecreasingMoney(money); //odeme yaptik
+                upgradeLevelName.text = "Upgrade Lvl. " + (upgradeLevel + 1); //level ismimizi guncelledik
+                upgradeMoney.text = UpgradeMoneyArray[upgradeLevel].ToString(); //paramizi guncelledik
+                Debug.Log(upgradeLevel);
+                upgradeLevel += 1; //bu kismmi kaydetmem lazim***.
+            }
+            
+        }
+        else
+        {
+            moneyShake(); //paramiz yoksa para kutucugunu titrestiriyoruz
+        }
+
+        return upgradeLevel;
+    }
+
+    public void moneyShake()
+    {
+        moneyBox.transform.DOShakeScale(0.5f, 0.2f, 10,90f,false);
+    }
     public void PlayerUpgrade()
     {
-        UserManager.Instance.DecreasingMoney(20);
-        playerUpgrade.text = _upgradeLevelName ;
+        _playerUpgradeLevel = UpgradeButton(PlayerUpgradeMoneyArray, _playerUpgradeLevel, playerUpgradeLevelName, playerUpgradeMoney);
     }
 
-    public void CarryUpgrade()
-    {
-        UserManager.Instance.DecreasingMoney(20);
-        carryUpgrade.text = "sdgsdg";
-    }
-    
     public void ShelverUpgrade()
     {
-        
+       _shelverUpgradeLevel = UpgradeButton(ShelverUpgradeMoneyArray, _shelverUpgradeLevel, shelverUpgradeLevelName, shelverUpgradeMoney);
+    }
+    
+    public void CarryUpgrade()
+    {
+        _carryUpgradeLevel = UpgradeButton(CarryUpgradeMoneyArray, _carryUpgradeLevel, carryUpgradeLevelName, carryUpgradeMoney);
     }
 
     public void PerfumeUpgrade()
     {
-        
+        _perfumeUpgradeLevel = UpgradeButton(PerfumeUpgradeMoneyArray, _perfumeUpgradeLevel, perfumeUpgradeLevelName, perfumeUpgradeMoney);
     }
 
     public void DelightUpgrade()
     {
-        
+        _delightUpgradeLevel = UpgradeButton(DelightUpgradeMoneyArray, _delightUpgradeLevel, delightUpgradeLevelName, delightUpgradeMoney);
     }
 }
